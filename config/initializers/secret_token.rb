f2dafe6605793df6d86f6e02260d7dfcb79aecbc
Adam Gilgoff <adam.gilg@gmail.com>
2013-04-09 15:48:39 -0700

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FindWorkFlow::Application.config.secret_token = 'b07e71721e0a7bb07e103682a7211edf342989fff5cc8ed2057cae8d5cf88c53049bcc3eb6fb29bfdedddecd34e990a6b015920986a109426b492a772c497957'
